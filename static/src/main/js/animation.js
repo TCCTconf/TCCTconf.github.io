aa766fae78f8ecfb316932de4b7a295f91c3f504
Oberon <st900278@gmail.com>
2014-11-15 16:58:12 +0800

    Add animation

--- static/src/main/js/animation.js
+++ static/src/main/js/animation.js
@@ -1,23 +1,25 @@
-jQuery(function ($) {
-	$.each($(".hex-group .hex").sort(function () {
-		return Math.random() * 10 > 5 ? 1 : -1;
-	}), function (index, element) {
-		var opacity = parseInt($(element).css('opacity'));
-		setTimeout(function () {
-			var tid = setInterval(function () {
-				opacity += 0.01;
-				$(element).css('opacity', opacity);
-				if (opacity > 1) clearInterval(tid);
-			}, 10);
-		}, index * 500);
+$(window).load(function () {
+	$(".loader").fadeOut(1000, function () {
+		$.each($(".hex-group .hex").sort(function () {
+			return Math.random() * 10 > 5 ? 1 : -1;
+		}), function (index, element) {
+			var opacity = parseInt($(element).css('opacity'));
+			setTimeout(function () {
+				var tid = setInterval(function () {
+					opacity += 0.01;
+					$(element).css('opacity', opacity);
+					if (opacity > 1) clearInterval(tid);
+				}, 10);
+			}, index * 500);
 
-	});
-	
-	$('header a').click(function(){
-		console.log("test");
-		$('html, body').animate({
-			scrollTop: $( $(this).attr('href') ).offset().top
-		}, 500);
-		return false;
+		});
+
+		$('header a').click(function () {
+			console.log("test");
+			$('html, body').animate({
+				scrollTop: $($(this).attr('href')).offset().top
+			}, 500);
+			return false;
+		});
 	});
 });
